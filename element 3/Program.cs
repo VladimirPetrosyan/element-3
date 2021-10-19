cbfeecc917ac1c0b307496a5ac64d6c39adb9f8f
VladimirPetrosyan <petrosyan412@icloud.com>
2021-10-19 18:46:12 +0300

    Добавьте файлы проекта.

---
+++ element 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace element_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            /*Важно кое-что уточнить, в задании сказано, что программа должна выбрать из данных чисел, то есть 
+             * числа нужно самому придумать, или же имелось ввиду что числа пользователь должен вводить?*/
+            a = 234214;
+            b = 324;
+            c = 3;
+            if(a == 1||a == 2 || a == 3)
+            {
+                Console.WriteLine(a);
+            if (b == 1 ||b == 2||b == 3)
+                {
+                Console.WriteLine(b);
+            if (c == 1 || c == 2 || c == 3);
+                    Console.WriteLine(c);
+                }
+            }
+            
+        }
+    }
+}
